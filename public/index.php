5d38d62e5399c7ec59ec2aab1208e019b6d4fd9e
Mathis Medard <mathismedard.74@gmail.com>
2022-09-07 11:55:58 +0200

    Twig example

--- public/index.php
+++ public/index.php
@@ -1,1 +1,31 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once './../vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+
+$loader = new FilesystemLoader('./../templates');
+$twig = new Environment($loader, [
+    'cache' => false,//'./../cache',
+    'debug' => true,
+]);
+
+    $template = $twig->load('home.tpl.html');
+
+echo $template->render([
+    'title' => 'Home',
+    'name' => 'John Doe',
+    'age' => 30,
+    'items' => [
+        'item1',
+        'item2',
+        'item3',
+    ],
+]);
+
