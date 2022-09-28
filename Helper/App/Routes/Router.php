42950366510b6c821fd3b3aa69ebf43f2214e6a2
Mathis Medard <mathismedard.74@gmail.com>
2022-09-28 10:50:15 +0200

    Use group for prefixed routes

--- Helper/App/Routes/Router.php
+++ Helper/App/Routes/Router.php
@@ -17,6 +17,7 @@ class Router
     const METHOD_DELETE = 'DELETE';
 
     static private ?Router $router = null;
+    static private string $prefix = '';
 
     private array $routesGET;
     private array $routesPOST;
@@ -90,7 +91,7 @@ class Router
      */
     public function registerGet(string $route, string $controller, string $action): void
     {
-        $this->routesGET[$route] = new Route($route, $controller, $action, 'GET');
+        $this->routesGET[self::$prefix . $route] = new Route(self::$prefix . $route, $controller, $action, 'GET');
     }
 
     /*
@@ -98,7 +99,7 @@ class Router
      */
     public function registerPost(string $route, string $controller, string $action): void
     {
-        $this->routesPOST[$route] = new Route($route, $controller, $action, 'POST');
+        $this->routesPOST[self::$prefix . $route] = new Route(self::$prefix . $route, $controller, $action, 'POST');
     }
 
     /*
@@ -106,7 +107,7 @@ class Router
      */
     public function registerPut(string $route, string $controller, string $action): void
     {
-        $this->routesPUT[$route] = new Route($route, $controller, $action, 'PUT');
+        $this->routesPUT[self::$prefix . $route] = new Route(self::$prefix . $route, $controller, $action, 'PUT');
     }
 
     /*
@@ -114,7 +115,7 @@ class Router
      */
     public function registerDelete(string $route, string $controller, string $action): void
     {
-        $this->routesDELETE[$route] = new Route($route, $controller, $action, 'DELETE');
+        $this->routesDELETE[self::$prefix . $route] = new Route(self::$prefix . $route, $controller, $action, 'DELETE');
     }
 
     /*
@@ -193,6 +194,14 @@ class Router
             (new Controller())->redirect(substr($this->route, 0, -1));
         }
         return $this->route;
+    }
+
+    public static function group(string $prefix, callable $routes):void
+    {
+        $savePrefix = self::$prefix;
+        self::$prefix = self::$prefix . $prefix;
+        $routes();
+        self::$prefix = $savePrefix;
     }
 
     /**
