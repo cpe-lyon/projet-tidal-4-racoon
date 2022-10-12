348d8140d9a00af6f602be8972cd911e54935561
Martin <martin.jourjon42@gmail.com>
2022-10-12 12:12:13 +0200

    ajout html pour tableau résultat

---
+++ src/scripts/home/home.ctrl.js
@@ -0,0 +1,11 @@
+import HomeService from "./home.service.js";
+
+class HomeCtrl {
+
+    constructor() {
+
+    }
+}
+
+
+export default HomeCtrl;
---
+++ src/scripts/home/home.service.js
@@ -0,0 +1,7 @@
+import Service from "../service/service.js";
+
+class HomeService extends Service {
+
+}
+
+export default HomeService;
