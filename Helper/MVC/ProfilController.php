8c9704a20994b4093dc60b6dc600478776af4aa7
Youreastonefox <louisa.chd@gmail.com>
2022-10-14 09:09:51 +0200

    Style page du profil utilisateur

--- Helper/MVC/ProfilController.php
+++ Helper/MVC/ProfilController.php
@@ -6,7 +6,7 @@ use Helper\Twig\Page;
 
 class ProfilController extends Controller
 {
-    public bool $isConnected = false;
+    public bool $isConnected = true;
 
     public function profile(): Page
     {
---
+++ src/scripts/profile.js
@@ -0,0 +1,4 @@
+// Pour chaque champ, ne pas autoriser le submit si :
+// - Un champ est vide
+// - Le mdp ou mail de confirmation n'est pas identique à l'original
+// Après un submit Vérifier les formats des champs
